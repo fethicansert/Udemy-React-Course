7bbb83da121f0ba8061012e546206b39e6aa2847
unknown <fethisert0@gmail.com>
2024-09-30 13:42:54 +0300

    Header Created and Player Comp created

---
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -0,0 +1,14 @@
+import Player from "./comps/Player"
+function App() {
+
+  return <main>
+    <div id="game-container">
+      <ol id="players">
+        <Player name={'Player 1'} symbol={'X'}></Player>
+        <Player name={'Player 2'} symbol={'Y'}></Player>
+      </ol>
+    </div>
+  </main>
+}
+
+export default App
