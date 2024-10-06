5a118b9b57129f956fb968af6234a518f80fdc2e
unknown <fethisert0@gmail.com>
2024-10-06 17:57:35 +0300

    gameboard and some player logic added

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,13 +1,59 @@
+import GameBoard from "./comps/GameBoard"
+import Log from "./comps/Log";
 import Player from "./comps/Player"
 import { useState } from "react"
 function App() {
- 
+  const [activePlayer, setActivePlayer] = useState('X');
+  const [gameTurns, setGameTurns] = useState([]);
+
+  const handleSquare = (rowIndex, colIndex) => {
+
+
+    setActivePlayer(prev => prev === 'X' ? 'O' : 'X');
+    console.log(activePlayer);
+    
+    setGameTurns(prevTurns => {
+      let currentPlayer = 'X';
+
+      if (prevTurns.length > 0 && prevTurns[0].player === 'X') {
+        currentPlayer = 'O'
+      };
+
+
+      const updatedTurns = [
+        { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
+        ...prevTurns];
+
+      return updatedTurns;
+
+    })
+  }
+
+
   return <main>
     <div id="game-container">
-      <ol id="players">
-        <Player initialName={'player1'} symbol={'X'}></Player>
-        <Player initialName={'player2'} symbol={'Y'}></Player>
+      <ol id="players" className="highlight-player">
+
+        <Player
+          initialName={'player1'}
+          symbol={'X'}
+          isActive={activePlayer === 'X'}>
+        </Player>
+
+        <Player
+          initialName={'player2'}
+          symbol={'Y'}
+          isActive={activePlayer === 'O'}>
+        </Player>
+
       </ol>
+
+      <GameBoard
+        onSelectSquare={handleSquare}
+        turns ={gameTurns}
+      />
+
+      <Log />
     </div>
   </main>
 }
--- 07-tic-tac-toe-starting-project/src/comps/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/comps/Player.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const Player = ({ initialName, symbol }) => {
+const Player = ({ initialName, symbol, isActive}) => {
 
     const [isEditing, setIsEditing] = useState(false);
     const [playerName, setPlayerName] = useState(initialName)
@@ -10,7 +10,7 @@ const Player = ({ initialName, symbol }) => {
 
    
     return (
-        <li>
+        <li className={isActive ? 'active' : undefined}>
             <span className="player">
 
                 {
