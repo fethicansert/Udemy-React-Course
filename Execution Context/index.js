c2cc2f1a9eb352a4aeeeb5a5e254637ec9271b62
unknown <fethisert0@gmail.com>
2024-11-04 11:51:03 +0200

    exercising primtive vs Objects

--- Execution Context/index.js
+++ Execution Context/index.js
@@ -1,45 +1,82 @@
-// 'use strict';
 
-function calcAge(birthYear){
-    const age = 2024 - birthYear;
-    
+//Primitive Types
+let lastName = 'Williams';
+let oldLastName = lastName;
+lastName = 'Davis';
 
-    function printAge(){
-        let output = `${firstName}, you are ${age}, born in ${birthYear}`;
-        console.log(output);
-        
-        if(birthYear >= 1981 && birthYear <= 1996){
-            var millenal = true;
 
-            //Creating NEW variable with same name as outer scope's variable
-            const firstName = 'Steven'
 
-            //reassining outer scopre's variable
-            output = 'NEW OUTPUT';
-            const str = `Oh, and you're a millenal, ${firstName}`;
-            console.log(str);
-
-            function add(a, b){
-                return a + b;
-            }
-         
-           
-            console.log(output);
-            
-        }
-
-        console.log(output);
-    
-        console.log(millenal);
-    }
-
-    printAge();
-    return age;
+//reference Types
+const jessica = {
+    firstName: 'Jessica',
+    lastName: 'Williams',
+    age: 27
 };
 
 
 
-const firstName = "Jonas";
-calcAge(1991);
+const marriedJessica = jessica;
+marriedJessica.lastName = 'Davis';
+console.log("Before marriage:", jessica);
+console.log("Agter marriage:", marriedJessica);
+
+
+const jessica2 = {
+    firstName: 'Jessica',
+    lastName: 'Williams',
+    age: 27,
+    family: ['Alice', 'Bob']
+};
+
+// const jessicaCopy = Object.assign({}, jessica2);
+
+// jessicaCopy.firstName = "Davis";
+// jessica2.age = 12;
+
+// jessicaCopy.family.push("Fethi");
+
+
+// console.log(jessica2);
+// console.log(jessicaCopy);
+
+
+const deepCopy = JSON.parse(JSON.stringify(jessica2));
+
+
+deepCopy.family.push('Fethi');
+deepCopy.age = 100;
+deepCopy.firstName = 'Ali';
+deepCopy.lastName = 'Karadamar';
+console.log(jessica2);
+
+console.log(deepCopy);
+
+
+
+
+// const game = {
+//     gameName: "Lol",
+//     main: 'Zed'
+// }
+
+// const player1 = {
+//     playerName: 'Fethi',
+//     age: 28,
+//     game: game
+// }
+// console.log(player1);
+
+// const player2 = Object.assign({}, player1);
+// player2.playerName= 'Ali Karadamar';
+
+
+// console.log(player1);
+// console.log(player2);
+
+// player2.game.main = 'Jungle'
+
+// console.log(player1);
+// console.log(player2);
+
 
 
