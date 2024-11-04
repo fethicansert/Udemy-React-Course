9431dd6796d04643a8a7cd1a827f4db5c2985696
unknown <fethisert0@gmail.com>
2024-11-04 15:02:25 +0200

    array destructuring and exercises

--- Execution Context/index.js
+++ Execution Context/index.js
@@ -1,82 +1,110 @@
+import { books } from "./booksData.js";
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
 
-//Primitive Types
-let lastName = 'Williams';
-let oldLastName = lastName;
-lastName = 'Davis';
-
-
-
-//reference Types
-const jessica = {
-    firstName: 'Jessica',
-    lastName: 'Williams',
-    age: 27
-};
+const [x, y, z] = arr;
 
 
 
-const marriedJessica = jessica;
-marriedJessica.lastName = 'Davis';
-console.log("Before marriage:", jessica);
-console.log("Agter marriage:", marriedJessica);
+console.log(x, y, z);
+
+const setState = () => {
+    return [10, 11];
+}
 
 
-const jessica2 = {
-    firstName: 'Jessica',
-    lastName: 'Williams',
-    age: 27,
-    family: ['Alice', 'Bob']
-};
+const [state, statez] = setState();
 
-// const jessicaCopy = Object.assign({}, jessica2);
+console.log(state);
+console.log(statez)
 
-// jessicaCopy.firstName = "Davis";
-// jessica2.age = 12;
+const resturant = {
+    name: "Classico Italiano",
+    location: "Via Angelo Tavanti 23, Firenze, Italy",
+    categories: ['Italian', 'Pizzeria', 'Vegeterian', 'Organic'],
+    starterMenu: ['Foccacia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+    mainMenu: ['Pizaa', 'Pasta', 'Risotto'],
+    openingHours: {
+        thu: {
+            open:12,
+            close:22
+        },
+        fri: {
+            open: 11,
+            close:23
+        },
+        sat: {
+            open: 0,
+            close: 24
+        }
+    },
+    order: function (starterIndex, mainIndex) {
+        return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+    }
+}
 
-// jessicaCopy.family.push("Fethi");
+// let [main, , , secondary] = resturant.categories;
+// console.log(main, secondary);
 
-
-// console.log(jessica2);
-// console.log(jessicaCopy);
-
-
-const deepCopy = JSON.parse(JSON.stringify(jessica2));
-
-
-deepCopy.family.push('Fethi');
-deepCopy.age = 100;
-deepCopy.firstName = 'Ali';
-deepCopy.lastName = 'Karadamar';
-console.log(jessica2);
-
-console.log(deepCopy);
-
-
-
-
-// const game = {
-//     gameName: "Lol",
-//     main: 'Zed'
-// }
-
-// const player1 = {
-//     playerName: 'Fethi',
-//     age: 28,
-//     game: game
-// }
-// console.log(player1);
-
-// const player2 = Object.assign({}, player1);
-// player2.playerName= 'Ali Karadamar';
+//Switching variables
+// const temp = main;
+// main = secondary;
+// secondary = temp;
 
 
-// console.log(player1);
-// console.log(player2);
+//Switching variables
+// [main, secondary] = [secondary, main];
+// console.log(main, secondary);
 
-// player2.game.main = 'Jungle'
 
-// console.log(player1);
-// console.log(player2);
+//Destructaring Arrays
+
+const [starter, main] = resturant.order(2, 0);
+console.log(starter, main);
+
+
+//Nested Destructaring
+const nested = [2, 4, [5, 6]];
+const [x1, , [z1, z2]] = nested;
+console.log(x1, z1, z2);
+const [i, , [j1, j2]] = nested;
+
+
+
+//Default values in array Destructaring
+
+const [p = 1, q = 2, r = 3] = [8, 9];
+console.log(p, q, r);
+
+
+
+console.log(books);
+
+
+const [firstBook, secondBook] = books;
+console.log(firstBook, secondBook);
+const [, , thirdBook] = books
+console.log(thirdBook);
+
+const ratings = [['rating', 4.19], ['ratingsCount', 144584]];
+
+const [[,rating], [,ratingCount]] = ratings;
+
+console.log(rating);
+console.log(ratingCount);
+
+
+const ratingStars = [63405, 1808];
+
+const [fiveStarRatings, oneStarRatings, threeStarRatings= 0] = ratingStars; 
+
+console.log(fiveStarRatings,oneStarRatings, threeStarRatings);
+
+
+
+
 
 
 
